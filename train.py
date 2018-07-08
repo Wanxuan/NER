5b6f7648850cbaad4088394f13db684d340b17c8
Wanxuan <sweetxuan16@gmail.com>
2018-07-08 19:57:47 +0800

    Update and rename text_generation.py to train.py

--- train.py
+++ train.py
@@ -55,25 +55,3 @@ checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only
 callbacks_list = [checkpoint]
 # fit the model
 model.fit(X, y, epochs=50, batch_size=64, callbacks=callbacks_list)
-
-# Load the network weights
-filename = "weights-improvement-19-1.9435.hdf5"
-model.load_weights(filename)
-model.compile(loss='categorical_crossentropy', optimizer='adam')
-# pick a random seed
-start = numpy.random.randint(0, len(dataX)-1)
-pattern = dataX[start]
-print("Seed:")
-print("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
-# generate characters
-for i in range(1000):
-    x = numpy.reshape(pattern, (1, len(pattern), 1))
-    x = x / float(n_vocab)
-    prediction = model.predict(x, verbose=0)
-    index = numpy.argmax(prediction)
-    result = int_to_char[index]
-    seq_in = [int_to_char[value] for value in pattern]
-    sys.stdout.write(result)
-    pattern.append(index)
-    pattern = pattern[1:len(pattern)]
-print("\nDone.")
