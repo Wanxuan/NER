1989c5752cbcfb63b8b72ab2577142d8c73c38f9
Wanxuan <sweetxuan16@gmail.com>
2018-07-08 19:56:04 +0800

    Create text.py

---
+++ text.py
@@ -0,0 +1,21 @@
+# Load the network weights
+filename = "weights-improvement-19-1.9435.hdf5"
+model.load_weights(filename)
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+# pick a random seed
+start = numpy.random.randint(0, len(dataX)-1)
+pattern = dataX[start]
+print("Seed:")
+print("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
+# generate characters
+for i in range(1000):
+    x = numpy.reshape(pattern, (1, len(pattern), 1))
+    x = x / float(n_vocab)
+    prediction = model.predict(x, verbose=0)
+    index = numpy.argmax(prediction)
+    result = int_to_char[index]
+    seq_in = [int_to_char[value] for value in pattern]
+    sys.stdout.write(result)
+    pattern.append(index)
+    pattern = pattern[1:len(pattern)]
+print("\nDone.")
