b3e4b2ccda6a3b8d870349a7a710a9d06bb399af
Wanxuan <sweetxuan16@gmail.com>
2018-08-02 13:40:37 +0800

    add model.save()

--- word2vec.py
+++ word2vec.py
@@ -2,5 +2,6 @@ import logging
 import os
 from gensim.models import word2vec
 logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s',level=logging.INFO)
-sentences = word2vec.LineSentence('corpusSegDone.txt')
+sentences = word2vec.LineSentence('/home/wanxuanl/corpusSegDone.txt')
 model = word2vec.Word2Vec(sentences,hs=1,min_count=1,window=4,size=300)
+model.save('/home/wanxuanl/')
