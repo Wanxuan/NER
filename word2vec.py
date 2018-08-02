7f8499606c8defcaa9b355d238aeea373e857988
Wanxuan <sweetxuan16@gmail.com>
2018-08-02 13:41:40 +0800

    change save path to /home/wanxuanl/mymodel

--- word2vec.py
+++ word2vec.py
@@ -4,4 +4,4 @@ from gensim.models import word2vec
 logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s',level=logging.INFO)
 sentences = word2vec.LineSentence('/home/wanxuanl/corpusSegDone.txt')
 model = word2vec.Word2Vec(sentences,hs=1,min_count=1,window=4,size=300)
-model.save('/home/wanxuanl/')
+model.save('/home/wanxuanl/mymodel')
