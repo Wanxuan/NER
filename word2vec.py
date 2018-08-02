709c3ce997e1368284e47086bd12fe8aeb9fe65a
Wanxuan <sweetxuan16@gmail.com>
2018-08-02 13:48:25 +0800

    add model.save_word2vec_format

--- word2vec.py
+++ word2vec.py
@@ -4,4 +4,6 @@ from gensim.models import word2vec
 logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s',level=logging.INFO)
 sentences = word2vec.LineSentence('/home/wanxuanl/corpusSegDone.txt')
 model = word2vec.Word2Vec(sentences,hs=1,min_count=1,window=4,size=300)
+model.train(sentences,total_examples=model.corpus_count,epochs=model.iter)
 model.save('/home/wanxuanl/mymodel')
+model.save_word2vec_format('home/wanxuanl/mymodel.txt,binary=False)
